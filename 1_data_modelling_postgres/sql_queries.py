04b34d04418b69e2782ad37231a677088b58aae1
rbuerki <r2d4@bluewin.ch>
2020-03-20 22:16:19 +0100

    initial commit

--- 1_data_modelling_postgres/sql_queries.py
+++ 1_data_modelling_postgres/sql_queries.py
@@ -8,54 +8,132 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = "CREATE TABLE IF NOT EXISTS songplays (\
-        songplay_id int, start_time numeric, user_id int, level int,\
-        song_id varchar, artist_id varchar, session_id int, location varchar, \
-        user_agent varchar);"
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
+                                          (songplay_id int,
+                                           start_time decimal,
+                                           user_id int,
+                                           level varchar,
+                                           song_id varchar,
+                                           artist_id varchar,
+                                           session_id int,
+                                           location varchar,
+                                           user_agent varchar
+                                           );
+""")
 
-user_table_create = "CREATE TABLE IF NOT EXISTS users (\
-        user_id int, first_name varchar, last_name varchar, gender bytea,\
-        level bytea);"
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users
+                                   (user_id int,
+                                    first_name varchar,
+                                    last_name varchar,
+                                    gender varchar,
+                                    level varchar
+                                    );
+""")
 
-song_table_create = "CREATE TABLE IF NOT EXISTS songs (\
-        song_id varchar, title varchar, artist_id varchar, year int,\
-        duration numeric);"
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs
+                                   (song_id varchar,
+                                    title varchar,
+                                    artist_id varchar,
+                                    year int,
+                                    duration decimal
+                                    );
+""")
 
-artist_table_create = "CREATE TABLE IF NOT EXISTS artists (\
-        artist_id varchar, name varchar, location varchar, lattitude numeric,\
-        longitude numeric);"
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
+                                   (artist_id varchar,
+                                    name varchar,
+                                    location varchar,
+                                    latitude decimal,
+                                    longitude decimal
+                                    );
+""")
 
-time_table_create = "CREATE TABLE IF NOT EXISTS users (\
-        start_time date, hour int, day int,week int, month int, year int,\
-        weekday boolean);"
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time
+                                   (start_time timestamp,
+                                    hour int,
+                                    day int,
+                                    week int,
+                                    month int,
+                                    year int,
+                                    weekday int
+                                    );
+""")
 
 # INSERT RECORDS
 
-songplay_table_insert = "INSERT INTO songplays (songplay_id, start_time,\
-                user_id, level, song_id, artist_id, session_id, location,\
-                user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+songplay_table_insert = ("""INSERT INTO songplays
+                                (start_time,
+                                 user_id,
+                                 level,
+                                 song_id,
+                                 artist_id,
+                                 session_id,
+                                 location,
+                                 user_agent)
+                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+""")
 
-user_table_insert = "INSERT INTO users (user_id, first_name, last_name,\
-                gender, level) VALUES (%s, %s, %s, %s, %s);"
+user_table_insert = ("""INSERT INTO users
+                            (userId,
+                             firstName,
+                             lastName,
+                             gender,
+                             level)
+                        VALUES (%s, %s, %s, %s, %s);
+""")
 
-song_table_insert = "INSERT INTO songs (song_id, title, artist_id, year,\
-                duration) VALUES (%s, %s, %s, %s, %s);"
+song_table_insert = ("""INSERT INTO songs
+                            (song_id,
+                             title,
+                             artist_id,
+                             year,
+                             duration)
+                        VALUES (%s, %s, %s, %s, %s);
+""")
 
-artist_table_insert = "INSERT INTO artists (artist_id, name, location,\
-                lattitude, longitude) VALUES (%s, %s, %s, %s, %s);"
+artist_table_insert = ("""INSERT INTO artists
+                            (artist_id,
+                             name,
+                             location,
+                             latitude,
+                             longitude)
+                        VALUES (%s, %s, %s, %s, %s);
+""")
 
-time_table_insert = "INSERT INTO artists (start_time, hour, day, week, month,\
-                year, weekday) VALUES (%s, %s, %s, %s, %s);"
+time_table_insert = ("""INSERT INTO time
+                            (start_time,
+                             hour,
+                             day,
+                             week,
+                             month,
+                             year,
+                             weekday)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s);
+""")
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT s.song_id,
+                         s.artist_id
+                  FROM   songs as s
+                  JOIN   artists as at
+                    ON   at.artist_id = s.artist_id
+                  WHERE  s.title = %s
+                   AND   at.name = %s
+                   AND   s.duration = %s;
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create,\
-                song_table_create, artist_table_create, time_table_create]
-
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop,\
-                artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create,
+                        user_table_create,
+                        song_table_create,
+                        artist_table_create,
+                        time_table_create,
+                        ]
+drop_table_queries = [songplay_table_drop,
+                      user_table_drop,
+                      song_table_drop,
+                      artist_table_drop,
+                      time_table_drop,
+                      ]
