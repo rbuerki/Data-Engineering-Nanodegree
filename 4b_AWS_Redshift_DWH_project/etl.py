33eae945be69be9c95b4534c1cddf183a005afee
rbuerki <r2d4@bluewin.ch>
2020-04-09 21:01:48 +0200

    DEV write basic functions, WIP

--- 4b_AWS_Redshift_DWH_project/etl.py
+++ 4b_AWS_Redshift_DWH_project/etl.py
@@ -1,119 +1,30 @@
-import os
-import glob
-import pandas as pd
-import sql_queries as sql
-from db_connect import get_connection
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
 
 
-def process_song_file(cur, filepath):
-    """Process a given song file and load data to database."""
-    # Open song file
-    df = pd.read_json(filepath, lines=True)
-
-    # Insert a song record
-    song_cols = ["song_id", "title", "artist_id", "year", "duration"]
-    song_data = df[song_cols].values[0].tolist()
-    cur.execute(sql.song_table_insert, song_data)
-
-    # Insert an artist record
-    artist_cols = ["artist_id",
-                   "artist_name",
-                   "artist_location",
-                   "artist_latitude",
-                   "artist_longitude",
-                   ]
-    artist_data = df[artist_cols].values[0].tolist()
-    cur.execute(sql.artist_table_insert, artist_data)
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
 
 
-def process_log_file(cur, filepath):
-    """Process a given log file and load data to database."""
-    # Open log file
-    df = pd.read_json(filepath, lines=True)
-    # Filter by NextSong action
-    df = df.loc[df["page"] == "NextSong"]
-    # Convert timestamp column to datetime
-    df["ts"] = pd.to_datetime(df["ts"], unit="ms")
-
-    # Extract data for time table
-    time_data = [df["ts"],
-                 df["ts"].dt.hour,
-                 df["ts"].dt.day,
-                 df["ts"].dt.week,
-                 df["ts"].dt.month,
-                 df["ts"].dt.year,
-                 df["ts"].dt.weekday,
-                 ]
-    cols = ["timestamp",
-            "hour",
-            "day",
-            "week",
-            "month",
-            "year",
-            "weekday",
-            ]
-    time_df = pd.DataFrame(dict(zip(cols, time_data)))
-    # Insert data for time table
-    for i, row in time_df.iterrows():
-        cur.execute(sql.time_table_insert, list(row))
-
-    # Load user table (simply select the respective columns)
-    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
-    # Insert user records
-    for i, row in user_df.iterrows():
-        cur.execute(sql.user_table_insert, row)
-
-    # Insert songplay records
-    for index, row in df.iterrows():
-        # Get songid and artistid from song and artist tables
-        cur.execute(sql.song_select, (row["song"],
-                                      row["artist"],
-                                      row["length"]
-                                      )
-                    )
-        results = cur.fetchone()
-        if results:
-            songid, artistid = results
-        else:
-            songid, artistid = None, None
-        # Insert songplay record
-        songplay_data = [row["ts"],
-                         row["userId"],
-                         row["level"],
-                         songid,
-                         artistid,
-                         row["sessionId"],
-                         row["location"],
-                         row["userAgent"],
-                         ]
-        cur.execute(sql.songplay_table_insert, songplay_data)
-
-
-def process_data(cur, conn, filepath, func):
-    """Process each data file in a give filepath using the passed function."""
-    # Get all files matching extension from directory
-    all_files = []
-    for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, '*.json'))
-        for f in files :
-            all_files.append(os.path.abspath(f))
-
-    # Get total number of files found
-    num_files = len(all_files)
-    print(f"{num_files} files found in {filepath}.")
-
-    # Iterate over files and process
-    for i, datafile in enumerate(all_files, 1):
-        func(cur, datafile)
-        # conn.commit()
-        print(f"{i}/{num_files} files processed.")
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
 
 
 def main():
-    conn, cur = get_connection()
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}"
+                            .format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
 
     conn.close()
 
--- 4b_AWS_Redshift_DWH_project/sql_queries.py
+++ 4b_AWS_Redshift_DWH_project/sql_queries.py
@@ -1,5 +1,14 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
 # DROP TABLES
 
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
 songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
 user_table_drop = "DROP TABLE IF EXISTS users;"
 song_table_drop = "DROP TABLE IF EXISTS songs;"
@@ -7,6 +16,14 @@ artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
+
+staging_events_table_create = ("""CREATE TABLE IF NOT EXISTS staging_events
+
+""")
+
+staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs
+
+""")
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
                                           (songplay_id SERIAL PRIMARY KEY,
@@ -59,86 +76,58 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time
                                     );
 """)
 
-# INSERT RECORDS
+# STAGING TABLES
 
-songplay_table_insert = ("""INSERT INTO songplays
-                                (start_time,
-                                 user_id,
-                                 level,
-                                 song_id,
-                                 artist_id,
-                                 session_id,
-                                 location,
-                                 user_agent)
-                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+staging_events_copy = ("""
+""").format()
+
+staging_songs_copy = ("""
+""").format()
+
+# FINAL TABLES
+
+songplay_table_insert = ("""
 """)
 
-user_table_insert = ("""INSERT INTO users
-                            (user_id,
-                             first_name,
-                             last_name,
-                             gender,
-                             level)
-                        VALUES (%s, %s, %s, %s, %s)
-                        ON CONFLICT (user_id) DO UPDATE
-                        SET level = EXCLUDED.level;
+user_table_insert = ("""
 """)
 
-song_table_insert = ("""INSERT INTO songs
-                            (song_id,
-                             title,
-                             artist_id,
-                             year,
-                             duration)
-                        VALUES (%s, %s, %s, %s, %s)
-                        ON CONFLICT DO NOTHING;
+song_table_insert = ("""
 """)
 
-artist_table_insert = ("""INSERT INTO artists
-                            (artist_id,
-                             name,
-                             location,
-                             latitude,
-                             longitude)
-                        VALUES (%s, %s, %s, %s, %s)
-                        ON CONFLICT DO NOTHING;
+artist_table_insert = ("""
 """)
 
-time_table_insert = ("""INSERT INTO time
-                            (start_time,
-                             hour,
-                             day,
-                             week,
-                             month,
-                             year,
-                             weekday)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s)
-                        ON CONFLICT DO NOTHING;
-""")
-
-# FIND SONGS
-
-song_select = ("""SELECT s.song_id,
-                         s.artist_id
-                  FROM   songs AS s
-                  JOIN   artists AS a
-                    ON   a.artist_id = s.artist_id
-                  WHERE  s.title = %s
-                    AND  a.name = %s
-                    AND  s.duration = %s;
+time_table_insert = ("""
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create,
+create_table_queries = [staging_events_table_create,
+                        staging_songs_table_create,
+                        songplay_table_create,
                         user_table_create,
                         song_table_create,
                         artist_table_create,
-                        time_table_create,
+                        time_table_create
                         ]
-drop_table_queries = [songplay_table_drop,
+
+drop_table_queries = [staging_events_table_drop,
+                      staging_songs_table_drop,
+                      songplay_table_drop,
                       user_table_drop,
                       song_table_drop,
-                      artist_table_drop,
-                      time_table_drop,
+                      artist_table_drop, 
+                      time_table_drop
                       ]
+
+copy_table_queries = [staging_events_copy,
+                      staging_songs_copy
+                      ]
+
+insert_table_queries = [songplay_table_insert,
+                        user_table_insert,
+                        song_table_insert,
+                        artist_table_insert,
+                        time_table_insert
+                        ]
