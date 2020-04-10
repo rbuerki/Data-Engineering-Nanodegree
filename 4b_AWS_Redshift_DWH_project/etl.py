633fb87f7b2500d8db9bda45bcef4c93e72adb95
rbuerki <r2d4@bluewin.ch>
2020-04-10 09:09:08 +0200

    DEV adapt connection logic

--- 4b_AWS_Redshift_DWH_project/etl.py
+++ 4b_AWS_Redshift_DWH_project/etl.py
@@ -1,32 +1,22 @@
-import configparser
-import psycopg2
+from db_connect import connect, close
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
-        conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
-        conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}"
-                            .format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-
+    cur, conn = connect()
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
-
-    conn.close()
+    close()
 
 
 if __name__ == "__main__":
