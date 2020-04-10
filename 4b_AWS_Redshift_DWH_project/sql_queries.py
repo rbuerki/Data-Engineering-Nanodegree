ec06271b13a8e2509e616fa3b8daa6126474d651
rbuerki <r2d4@bluewin.ch>
2020-04-10 08:40:17 +0200

    FEAT define all queries, add config params

--- 4b_AWS_Redshift_DWH_project/sql_queries.py
+++ 4b_AWS_Redshift_DWH_project/sql_queries.py
@@ -1,9 +1,15 @@
 import configparser
 
 
-# CONFIG
+# CONFIG (return dwh configuration in ini format)
 config = configparser.ConfigParser()
-config.read('dwh.cfg')
+config.read_file(open('dwh.cfg'))
+
+ARN = config.get("IAM_ROLE", "ARN")
+LOG_DATA = config.get("S3", "LOG_DATA")
+LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
+SONG_DATA = config.get("S3", "SONG_DATA")
+
 
 # DROP TABLES
 
@@ -20,60 +26,65 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # Postgres' SERIAL command is not supported in Redshift. The equivalent is IDENTITY(0,1)
 # Read more here: https://docs.aws.amazon.com/redshift/latest/dg/r_CREATE_TABLE_NEW.html
 
+# Staging Tables
+
 staging_events_table_create = ("""CREATE TABLE IF NOT EXISTS staging_events
-                                                    artist VARCHAR NOT NULL,
-                                                    auth VARCHAR NOT NULL,
-                                                    firstName VARCHAR NOT NULL,
-                                                    gender CHAR(1) NOT NULL,
-                                                    itemInSession INT NOT NULL,
-                                                    lastName VARCHAR NOT NULL,
-                                                    length DECIMAL NOT NULL,
-                                                    level VARCHAR NOT NULL,
-                                                    location VARCHAR NOT NULL,
-                                                    method VARCHAR NOT NULL,
-                                                    page VARCHAR NOT NULL,
-                                                    registration VARCHAR(15) NOT NULL,
-                                                    sessionId INT NOT NULL,
-                                                    song VARCHAR NOT NULL,
-                                                    status INT NOT NULL,
-                                                    ts TIMESTAMP NOT NULL,
+                                                    artist VARCHAR,
+                                                    auth VARCHAR,
+                                                    firstName VARCHAR,
+                                                    gender CHAR(1),
+                                                    itemInSession INT,
+                                                    lastName VARCHAR,
+                                                    length DECIMAL,
+                                                    level VARCHAR,
+                                                    location VARCHAR,
+                                                    method VARCHAR,
+                                                    page VARCHAR,
+                                                    registration VARCHAR(15),
+                                                    sessionId INT SORT KEY DIST KEY,
+                                                    song VARCHAR,
+                                                    status INT,
+                                                    ts TIMESTAMP,
                                                     userAgent VARCHAR,
-                                                    userId INT NOT NULL
+                                                    userId INT
 """)
 
 staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs
-                                                (num_songs IDENTITY(0,1),
-                                                 artist_id  VARCHAR(18) NOT NULL,
+                                                (artist_id  VARCHAR(18),
                                                  artist_latitude DECIMAL,
                                                  artist_longitude DECIMAL,
                                                  artist_location VARCHAR,
-                                                 artist_name VARCHAR NOT NULL,
-                                                 song_id VARCHAR(18) NOT NULL,
-                                                 title VARCHAR NOT NULL,
-                                                 duration DECIMAL NOT NULL,
-                                                 year INT NOT NULL
+                                                 artist_name VARCHAR,
+                                                 song_id VARCHAR(18) SORT KEY DIST KEY,
+                                                 title VARCHAR,
+                                                 duration DECIMAL,
+                                                 year INT
                                                  );
 """)
 
+# Fact Table
+
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
-                                          (songplay_id SERIAL PRIMARY KEY,
-                                           start_time TIMESTAMP NOT NULL,
+                                          (songplay_id IDENTITY(0,1) PRIMARY KEY,
+                                           start_time TIMESTAMP,
                                            user_id INT NOT NULL,
-                                           level VARCHAR NOT NULL,
+                                           level VARCHAR,
                                            song_id VARCHAR(18),
                                            artist_id VARCHAR(18),
                                            session_id INT NOT NULL,
-                                           location VARCHAR NOT NULL,
-                                           user_agent VARCHAR NOT NULL
+                                           location VARCHAR,
+                                           user_agent VARCHAR
                                            );
 """)
 
+# Dim Tables
+
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users
                                    (user_id INT PRIMARY KEY,
-                                    first_name VARCHAR NOT NULL,
-                                    last_name VARCHAR NOT NULL,
-                                    gender CHAR(1) NOT NULL,
-                                    level VARCHAR NOT NULL
+                                    first_name VARCHAR,
+                                    last_name VARCHAR,
+                                    gender CHAR(1),
+                                    level VARCHAR
                                     );
 """)
 
@@ -96,7 +107,7 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time
-                                   (start_time TIMESTAMP NOT NULL,
+                                   (start_time TIMESTAMP NOT NULL PRIMARY KEY,
                                     hour INT NOT NULL,
                                     day INT NOT NULL,
                                     week INT NOT NULL,
@@ -106,37 +117,125 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time
                                     );
 """)
 
-# STAGING TABLES
+# COPY INTO STAGING TABLES
+
+# COPY loads data into a table from data files or from an Amazon DynamoDB table.
+# Read more here: https://docs.aws.amazon.com/redshift/latest/dg/r_COPY.html
+# and here: https://docs.aws.amazon.com/redshift/latest/dg/copy-parameters-data-format.html#copy-json-jsonpaths
 
 staging_events_copy = (f"""
             COPY staging_events
-            FROM {LOG_DATA}    --------------------- what is this for: LOG_JSONPATH='s3://udacity-dend/log_json_path.json'
-            credentials 'aws_iam_role={ARN}'
-            gzip delimiter ';' region 'us-west-2';  ---------------------------- CHECK THE REGION!
+            FROM {LOG_DATA}
+            CREDENTIALS 'aws_iam_role={ARN}'
+            FORMAT AS JSON {LOG_JSONPATH}
+            TIMEFORMAT as 'epochmillisecs'
+            TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL
+            REGION 'us-west-2';
 """)
 
 staging_songs_copy = (f"""
             COPY staging_songs
             FROM {SONG_DATA}
-            credentials 'aws_iam_role={ARN}'
-            gzip delimiter ';' region 'us-west-2';
-""").format()
-
-# FINAL TABLES
-
-songplay_table_insert = ("""
+            CREDENTIALS 'aws_iam_role={ARN}'
+            JSON 'auto'
+            TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL
+            REGION 'us-west-2';
 """)
 
-user_table_insert = ("""
+# INSERT INTO FINAL TABLES
+
+songplay_table_insert = ("""INSERT INTO songplays
+                                (start_time,
+                                 user_id,
+                                 level,
+                                 song_id,
+                                 artist_id,
+                                 session_id,
+                                 location,
+                                 user_agent)
+                            SELECT
+                                 DISTINCT e.ts,
+                                 e.user_id,
+                                 e.level,
+                                 s.song_id,
+                                 s.artist_id,
+                                 e.session_id,
+                                 e.location,
+                                 e.user_agent
+                            FROM staging_event AS e
+                            JOIN staging_song AS s
+                                ON e.artist = s.artist_name
+                            WHERE e.page = 'NextSong'
 """)
 
-song_table_insert = ("""
+user_table_insert = ("""INSERT INTO users
+                            (user_id,
+                             first_name,
+                             last_name,
+                             gender,
+                             level)
+                        SELECT
+                             DISTINCT e.user_id,
+                             e.first_name,
+                             e.last_name,
+                             e.gender,
+                             e.level
+                        FROM staging_events AS e
+                        WHERE e.page = 'NextSong'
+                        AND e.user_id IS NOT NULL
 """)
 
-artist_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs
+                            (song_id,
+                             title,
+                             artist_id,
+                             year,
+                             duration)
+                        SELECT
+                             DISTINCT s.song_id
+                             s.title,
+                             s.artist_id,
+                             s.year,
+                             s.duration,
+                        FROM staging_songs AS s
+                        WHERE s.song_id IS NOT NULL
 """)
 
-time_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists
+                            (artist_id,
+                             name,
+                             location,
+                             latitude,
+                             longitude)
+                          SELECT
+                             DISTINCT s.artist_id,
+                             s.artist_name,
+                             s.artist_location,
+                             s.artist_latitude,
+                             s.artist_longitude
+                          FROM staging_songs AS s
+                          WHERE s.artist_id IS NOT NULL
+""")
+
+time_table_insert = ("""INSERT INTO time
+                            (start_time,
+                             hour,
+                             day,
+                             week,
+                             month,
+                             year,
+                             weekday)
+                        SELECT
+                             DISTINCT e.ts
+                             EXTRACT(hour FROM e.ts)
+                             EXTRACT(day FROM e.ts)
+                             EXTRACT(week FROM e.ts)
+                             EXTRACT(month FROM e.ts)
+                             EXTRACT(year FROM e.ts)
+                             EXTRACT(weekday FROM e.ts)
+                        FROM staging_events AS e
+                        WHERE page = 'NextSong'
+                        AND ts IS NOT NULL
 """)
 
 # QUERY LISTS
