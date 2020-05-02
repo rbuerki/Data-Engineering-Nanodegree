e8be30076fcf3e198d8dd50d23b2bd77d4fc7791
rbuerki <r2d4@bluewin.ch>
2020-05-02 21:39:00 +0200

    add destination_table param

--- 6_Apache_Airflow_Pipeline_project/plugins/operators/load_fact.py
+++ 6_Apache_Airflow_Pipeline_project/plugins/operators/load_fact.py
@@ -4,21 +4,36 @@ from airflow.utils.decorators import apply_defaults
 
 
 class LoadFactOperator(BaseOperator):
+    """Load data into fact table from staging tables.
 
+    Parameters:
+    -----------
+    - redshift_conn_id: Conn Id of the Airflow connection to redshift database
+    - destination_table: name of the fact table to update
+    - sql_statement: 'SELECT' query to retrieve rows for insertion in fact table
+
+    Returns:
+    --------
+    - None
+
+    """
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 sql_query="",
+                 destination_table="",
+                 sql_statement="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.sql_query = sql_query
+        self.destination_table = destination_table
+        self.sql_statement = sql_statement
 
     def execute(self, context):
         redshift = PostgresHook(self.redshift_conn_id)
+        sql_query = f"INSERT INTO {self.destination_table} ({self.sql_statement})"
 
         self.log.info('Loading data into fact table')
-        redshift.run(self.sql_query)
+        redshift.run(sql_query)
