13013393a7c3cb507e1bbe6f2589d61923d419be
rbuerki <r2d4@bluewin.ch>
2020-05-02 16:07:14 +0200

    rename class

--- 6_Apache_Airflow_Pipeline_project/plugins/__init__.py
+++ 6_Apache_Airflow_Pipeline_project/plugins/__init__.py
@@ -1,13 +1,12 @@
 from __future__ import division, absolute_import, print_function
-
 from airflow.plugins_manager import AirflowPlugin
-
 import operators
 import helpers
 
+
 # Defining the plugin class
-class UdacityPlugin(AirflowPlugin):
-    name = "udacity_plugin"
+class SparkifyPlugin(AirflowPlugin):
+    name = "sparkify_plugin"
     operators = [
         operators.StageToRedshiftOperator,
         operators.LoadFactOperator,
